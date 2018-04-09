4cb3020d291b85c2cb72288c616b94e2be3bec30
robithc <robithc@gmail.com>
2018-04-09 16:23:57 +0300

    update

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -3,11 +3,11 @@ package com.example.android.justjava;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
@@ -31,8 +31,15 @@ public class MainActivity extends AppCompatActivity {
         CheckBox whippedCreamCheckbox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
         Boolean hasWhippedCream = whippedCreamCheckbox.isChecked();
 
+        CheckBox chocolateCheckbox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        Boolean hasChocolate = chocolateCheckbox.isChecked();
+
+        EditText nameText = (EditText) findViewById(R.id.name_edit_text);
+        String editText = nameText.getText().toString();
+        Log.v("MainActivity", "editText:" + editText);
+
         int price = calculatePrice();
-        String priceMessage = createOrderSummary(price, hasWhippedCream);
+        String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate, editText);
         displayMessage(priceMessage);
 
 
@@ -79,14 +86,47 @@ public class MainActivity extends AppCompatActivity {
      * @return the price
      */
     private int calculatePrice() {
+        int coffeePrice = 5;
 
-        return quantity * 5;
+        int whippedCreamPrice = 1;
+        int chocolatePrice = 2;
+        CheckBox whippedCreamCheckbox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        Boolean hasWhippedCream = whippedCreamCheckbox.isChecked();
+
+        CheckBox chocolateCheckbox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        Boolean hasChocolate = chocolateCheckbox.isChecked();
+
+        int basePrice = coffeePrice * quantity;
+        int coffeePriceCream = (quantity * coffeePrice) + (whippedCreamPrice * quantity);
+        int coffeePriceChocolate = (quantity * coffeePrice) + (chocolatePrice * quantity);
+        int totalPrice = basePrice + coffeePriceCream;
+        if (hasWhippedCream) {
+            totalPrice = basePrice + coffeePriceCream;
+        } else if (hasChocolate) {
+            totalPrice = basePrice + coffeePriceChocolate;
+
+        } else {
+            return totalPrice;
+        }
+
+
+        return totalPrice;
 
     }
 
-    private String createOrderSummary(int price, boolean hasWhippedCream) {
-        String priceMessage = "Name: Kaptain Kunal";
+    /**
+     * Create summary of the order.
+     *
+     * @param hasWhippedCream is whether or not the user wants whipped cream topping
+     * @param hasChocolate    is whether or not the user wants chocolate topping
+     * @param price           of the order
+     * @return text summary
+     */
+
+    private String createOrderSummary(int price, boolean hasWhippedCream, boolean hasChocolate, String editText) {
+        String priceMessage = "Name: " + editText;
         priceMessage += "\nAdd whipped cream? " + hasWhippedCream;
+        priceMessage += "\nAdd chocolate? " + hasChocolate;
         priceMessage += "\nQuantity: " + quantity + "\nTotal: $" + price + "\nThank You!";
         return priceMessage;
 
